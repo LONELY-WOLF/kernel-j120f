2acda76839ea2d5c5b1dd916c4a12121ff6b3716
LONELY-WOLF <zvenayte@pisem.net>
2018-01-31 16:54:48 +0300

    Samsung AQE kernel

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -2112,13 +2112,9 @@ unsigned int snd_soc_read(struct snd_soc_codec *codec, unsigned int reg)
 {
 	unsigned int ret;
 
-	if (codec->read) {
-		ret = codec->read(codec, reg);
-		dev_dbg(codec->dev, "read %x => %x\n", reg, ret);
-		trace_snd_soc_reg_read(codec, reg, ret);
-	}
-	else
-		ret = -1;
+	ret = codec->read(codec, reg);
+	dev_dbg(codec->dev, "read %x => %x\n", reg, ret);
+	trace_snd_soc_reg_read(codec, reg, ret);
 
 	return ret;
 }
@@ -2127,13 +2123,9 @@ EXPORT_SYMBOL_GPL(snd_soc_read);
 unsigned int snd_soc_write(struct snd_soc_codec *codec,
 			   unsigned int reg, unsigned int val)
 {
-	if (codec->write) {
-		dev_dbg(codec->dev, "write %x = %x\n", reg, val);
-		trace_snd_soc_reg_write(codec, reg, val);
-		return codec->write(codec, reg, val);
-	}
-	else
-		return -1;
+	dev_dbg(codec->dev, "write %x = %x\n", reg, val);
+	trace_snd_soc_reg_write(codec, reg, val);
+	return codec->write(codec, reg, val);
 }
 EXPORT_SYMBOL_GPL(snd_soc_write);
 
